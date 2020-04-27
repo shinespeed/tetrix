b6bb21c0c2263574da083f1a974dd8041f8d4d40
shinespeed <42372037+shinespeed@users.noreply.github.com>
2020-04-27 16:30:51 +0300

    Update Game.cpp

--- Game.cpp
+++ Game.cpp
@@ -1,5 +1,3 @@
-// Copyright 2009-2014 Blam BaseApps, Inc. All Rights Reserved.
-
 #include "Game.h"
 
 enum numberFigure
@@ -13,24 +11,30 @@ enum numberFigure
 	SFIGURE
 };
 
-Game::Game():
-	Parent(40, 20),
+Game::Game() :
+	BaseApp(WINDOW_SIZE_X, WINDOW_SIZE_Y),
 	mDirection(true),
 	timeStep(0),
 	resolutionCreateFigure(true),
 	moveX(START_COORD_X),
-	moveY(START_COORD_Y)
+	moveY(START_COORD_Y),
+	furureFigure(randomFigure())
 {}
+
+Game::~Game() 
+{
+	if (furureFigure != nullptr) delete furureFigure;
+}
 
 void Game::KeyPressed(int btnCode)
 {
-	if (btnCode == 32) //
+	if (btnCode == 32) 
 	{
 		world.rotationFigure();
 	}
-	else if ((btnCode == 115)&&(world.regulationFigure(moveX, moveY + 1))) //s
+	else if ((btnCode == 115)&&(world.regulationFigure(moveX, moveY + 1))) 
 	{
-
+		--moveY;
 	}
 	else if ((btnCode == 75) && (world.regulationFigure(moveX - 1, moveY))) //a
 	{
@@ -44,66 +48,48 @@ void Game::KeyPressed(int btnCode)
 	}
 }
 
-void Game::graphBorders() 
+void Game::gameOverCheck(Figure* figure)
 {
-	for (int i = 0; i <= BORDER_END_X; i++)
-	{
-		for (int j = 0; j <= BORDER_END_Y; j++)
-		{
-			if ( i == 0 || j == 0 || i == BORDER_END_X || j == BORDER_END_Y || (i == BORDER_1_X)&&(j <= BORDER_1_Y) || (j == BORDER_2_Y))
-			{
-				SetChar(i, j, L'#');
-			}
-			else
-			{
-				//SetChar(i, j, 10);
-			}
-		}
-	}
-}
+	int tempX(0);
+	int tempY(0);
 
-void Game::Render() 
-{
-	for (int i = 0; i < ARRAY_Y; i++) 
+	for (int i = 0; i < QUANTITY_SQUARE; i++) 
 	{
-		for (int j = 0; j < ARRAY_X; j++) 
+		tempX = figure->getArrSquareFigure(i)->xSquare;
+		tempY = figure->getArrSquareFigure(i)->ySquare;
+		if (world.arrSquare[tempY][tempX] != nullptr) 
 		{
-			if (world.arrSquare[i][j] == nullptr) 
-			{
-				SetChar(j + PLAYING_FIELD_X, i + PLAYING_FIELD_X, L'.');
-			}
-			else 
-			{
-				SetChar(j + PLAYING_FIELD_X, i + PLAYING_FIELD_Y, 10);
-			}
+			gameCheck = false;
 		}
 	}
-	graphBorders();
 }
 
 void Game::createFigure()
 {
 	if (resolutionCreateFigure == true)
 	{
-		world.addFigure(randomFigure());
+		gameOverCheck(furureFigure);
+
+		world.checkSumSquareHorizon();
+		world.addFigure(furureFigure);
 
 		moveX = START_COORD_X;
 		moveY = START_COORD_Y;
 
+		furureFigure = randomFigure();
+		renderFutureFigure();
 		resolutionCreateFigure = false;
 	}
 }
 
 Figure* Game::randomFigure() 
-{
-	srand(time(NULL));                   
-
+{     
 	int random = rand() % 7;
 
 	switch (random)
 	{
 	case TFIGURE:
-		return new IFigure(START_COORD_X, START_COORD_Y);
+		return new TFigure(START_COORD_X, START_COORD_Y);
 		break;
 	case QFIGURE:
 		return new QFigure(START_COORD_X, START_COORD_Y);
@@ -145,10 +131,68 @@ void Game::start()
 	}
 }
 
+void Game::renderFutureFigure() 
+{
+	int tempX(0);
+	int tempY(0);
+
+	for (int i = 0; i < QUANTITY_SQUARE; i++)
+	{
+		for (int j = 0; j < QUANTITY_SQUARE; j++)
+		{
+			SetChar(i + FUTURE_FIGURE_X - 1, j + FUTURE_FIGURE_Y, 0);
+		}
+	}
+
+	for (int i = 0; i < QUANTITY_SQUARE; i++) 
+	{
+
+		tempX = furureFigure->getArrSquareFigure(i)->xSquare - START_COORD_X;
+		tempY = furureFigure->getArrSquareFigure(i)->ySquare - START_COORD_Y;
+
+		SetChar(tempX + FUTURE_FIGURE_X, tempY + FUTURE_FIGURE_Y,  10);
+	}
+}
+
+void Game::graphBorders()
+{
+	for (int i = BORDER_START_X; i <= BORDER_END_X; i++)
+	{
+		for (int j = BORDER_START_Y; j <= BORDER_END_Y; j++)
+		{
+			if (i == BORDER_START_X || j == BORDER_START_Y || i == BORDER_END_X || j == BORDER_END_Y || (i == BORDER_1_X) && (j <= BORDER_1_Y) || (j == BORDER_2_Y))
+			{
+				SetChar(i, j, L'#');
+			}
+		}
+	}
+}
+
+void Game::Render()
+{
+	graphBorders();
+	SetString(3, 19, "> Point: ");
+	SetString(14, 19, std::to_string(world.sumPoint));
+
+	for (int i = 0; i < ARRAY_Y; ++i)
+	{
+		for (int j = 0; j < ARRAY_X; ++j)
+		{
+			if (world.arrSquare[i][j] == nullptr)
+			{
+				SetChar(j + PLAYING_FIELD_X, i + PLAYING_FIELD_Y, L'.');
+			}
+			else
+			{
+				SetChar(j + PLAYING_FIELD_X, i + PLAYING_FIELD_Y, 10);
+			}
+		}
+	}
+}
+
 void Game::Update(float deltaTime)
 {
 	timeStep += deltaTime;
-
-        start();
+    start();
 	Render();
 }
