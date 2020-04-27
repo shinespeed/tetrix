4a9f68f09382f1de3e56d087d54b0ba2e3f62c59
shinespeed <42372037+shinespeed@users.noreply.github.com>
2020-04-27 07:36:12 +0300

    update

--- World.cpp
+++ World.cpp
@@ -1,23 +1,80 @@
 #include "World.h"
 
 
-World::World()
-{
-}
+World::World(){}
 
 World::~World()
 {
-	//delete[] arrSquare;
+	delete[] arrSquare;
 }
 
-void World::matrixRot(int index) 
+bool World::regulationFigure(int futureX, int futureY)
 {
 	int tempX = 0;
 	int tempY = 0;
+	int numberViolation = 0;
 
-	tempX = tempFigure->getArrSquareFigure(0)->xSquare + tempFigure->getArrSquareFigure(0)->ySquare - tempFigure->getArrSquareFigure(index)->ySquare;
-	tempY = tempFigure->getArrSquareFigure(index)->xSquare + tempFigure->getArrSquareFigure(0)->ySquare - tempFigure->getArrSquareFigure(0)->xSquare;
+	for (int i = 0; i < QUANTITY_SQUARE; i++)
+	{
+	    tempX = tempFigure->getArrSquareFigure(i)->xSquare + (futureX - tempFigure->getXFigure());
+		tempY = tempFigure->getArrSquareFigure(i)->ySquare + (futureY - tempFigure->getYFigure());
 
+		if (!regulationSquare(tempX, tempY)) ++numberViolation;
+	} 
+	if (numberViolation == 0) 
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+
+bool World::regulationSquare(int squarefutureX, int squarefutureY)
+{
+	if (squarefutureX >= 0 && squarefutureX < ARRAY_X && squarefutureY < ARRAY_Y)
+	{
+		if (arrSquare[squarefutureY][squarefutureX] == nullptr) 
+		{
+			return true;
+		}
+		else
+		{
+			return whoseSquare(squarefutureX, squarefutureY);
+		}
+	} 
+	else return false;
+}
+
+bool World::whoseSquare(int squarefutureX, int squarefutureY)
+{
+	bool squareFigure = false;
+
+	for (int i = 0; i < QUANTITY_SQUARE; ++i)
+	{
+		if (arrSquare[squarefutureY][squarefutureX] == tempFigure->getArrSquareFigure(i))
+		{
+			squareFigure = true;
+			break;
+		}
+	}
+	return squareFigure;
+}
+
+void World::calculationFutureXY(int &futureX, int &futureY, const int index)
+{
+	futureX = tempFigure->getArrSquareFigure(0)->xSquare + tempFigure->getArrSquareFigure(0)->ySquare - tempFigure->getArrSquareFigure(index)->ySquare;
+	futureY = tempFigure->getArrSquareFigure(index)->xSquare + tempFigure->getArrSquareFigure(0)->ySquare - tempFigure->getArrSquareFigure(0)->xSquare;
+}
+
+void World::rotationMatrixSquare(const int index, int &numberViolation)
+{
+	int tempX(0);
+	int tempY(0);
+
+	calculationFutureXY(tempX, tempY, index);
+	
 	arrSquare[tempY][tempX] = tempFigure->getArrSquareFigure(index);
 
 	arrSquare[tempFigure->getArrSquareFigure(index)->ySquare][tempFigure->getArrSquareFigure(index)->xSquare] = nullptr;
@@ -28,9 +85,29 @@ void World::matrixRot(int index)
 
 void World::rotationFigure()
 {
-	matrixRot(1);
-	matrixRot(2);
-	matrixRot(3);
+	int numberViolation(0);
+	int tempX(0);
+	int tempY(0);
+
+	for (int i = 1; i < QUANTITY_SQUARE; ++i)
+	{
+		calculationFutureXY(tempX, tempY, i);
+		
+		if (!regulationSquare(tempX, tempY))
+		{ 
+			++numberViolation; 
+			break;
+		}
+	}
+
+	if (numberViolation == 0) 
+	{
+		for (int i = 1; i < QUANTITY_SQUARE; ++i) 
+		{
+			rotationMatrixSquare(i, numberViolation);
+		}
+	}
+
 	moveFigure(tempFigure->getXFigure(), tempFigure->getYFigure());
 }
 
