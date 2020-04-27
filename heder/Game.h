5f4601c68234f0cf3418e3d71dd5ea5ef50d3bee
shinespeed <42372037+shinespeed@users.noreply.github.com>
2020-04-27 16:34:32 +0300

    Update Game.h

--- heder/Game.h
+++ heder/Game.h
@@ -1,7 +1,7 @@
-// Copyright 2009-2014 Blam BaseApps, Inc. All Rights Reserved.
+#ifndef GAME_H
+#define GAME_H
 
-#pragma once
-
+#include <thread>
 #include <cstdlib>
 #include <ctime>
 #include "GlobalConstant.h"
@@ -11,23 +11,14 @@
 
 class Game : public BaseApp
 {
-	typedef BaseApp Parent;
-
 public:
 	Game();
+	~Game();
 
-	virtual void KeyPressed(int btnCode);
-	virtual void Update(float deltaTime);
-
+	void KeyPressed(int btnCode);
+	void Update(float deltaTime);
 private:
 	World world;
-
-	void Render();
-	void start();
-	void graphBorders();
-
-	void createFigure();
-	Figure* randomFigure();
 
 	bool resolutionCreateFigure;
 	bool mDirection;
@@ -36,4 +27,16 @@ private:
 	int moveY;
 
 	float timeStep;
+
+	Figure* randomFigure();
+	Figure* furureFigure;
+
+	void gameOverCheck(Figure* figure);
+	void createFigure();
+	void start();
+	void renderFutureFigure();
+	void graphBorders();
+	void Render();
 };
+
+#endif
