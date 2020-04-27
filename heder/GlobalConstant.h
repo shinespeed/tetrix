685b2c818ccea7bad7eddbb1c34fe5c5a02b2a10
shinespeed <42372037+shinespeed@users.noreply.github.com>
2020-04-27 07:37:48 +0300

    Update GlobalConstant.h

--- heder/GlobalConstant.h
+++ heder/GlobalConstant.h
@@ -5,10 +5,27 @@ const int ARRAY_X = 15;
 const int ARRAY_Y = 15;
 
 //speed iteration game
-const float GAME_ITERATION = 0.8;
+const float GAME_ITERATION = 0.3;
 
 const int START_COORD_X = 7;
 const int START_COORD_Y = 0;
+
+const int QUANTITY_SQUARE = 4;
+
+//border playing field graph
+const int BORDER_PRIMARY_X = 0;
+const int BORDER_PRIMARY_Y = 0;
+
+const int BORDER_END_X = 25;
+const int BORDER_END_Y = 20;
+
+const int BORDER_1_X = 16;
+const int BORDER_1_Y = 15;
+
+const int BORDER_2_Y = 16;
+
+const int PLAYING_FIELD_X = 1;
+const int PLAYING_FIELD_Y = 1;
 
 //coord square figure 
 const int TFIGURE_COORD_1_X = 0;
@@ -20,6 +37,72 @@ const int TFIGURE_COORD_1_Y = 0;
 const int TFIGURE_COORD_2_Y = 0;
 const int TFIGURE_COORD_3_Y = 0;
 const int TFIGURE_COORD_4_Y = 1;
+
+//coord square figure 
+const int IFIGURE_COORD_1_X = 0;
+const int IFIGURE_COORD_2_X = 0;
+const int IFIGURE_COORD_3_X = 0;
+const int IFIGURE_COORD_4_X = 0;
+
+const int IFIGURE_COORD_1_Y = 0;
+const int IFIGURE_COORD_2_Y = 1;
+const int IFIGURE_COORD_3_Y = 2;
+const int IFIGURE_COORD_4_Y = 3;
+
+//coord square figure 
+const int QFIGURE_COORD_1_X = 0;
+const int QFIGURE_COORD_2_X = 1;
+const int QFIGURE_COORD_3_X = 0;
+const int QFIGURE_COORD_4_X = 1;
+
+const int QFIGURE_COORD_1_Y = 0;
+const int QFIGURE_COORD_2_Y = 0;
+const int QFIGURE_COORD_3_Y = 1;
+const int QFIGURE_COORD_4_Y = 1;
+
+//coord square figure 
+const int ZFIGURE_COORD_1_X = 0;
+const int ZFIGURE_COORD_2_X = -1;
+const int ZFIGURE_COORD_3_X = 0;
+const int ZFIGURE_COORD_4_X = 1;
+
+const int ZFIGURE_COORD_1_Y = 0;
+const int ZFIGURE_COORD_2_Y = 0;
+const int ZFIGURE_COORD_3_Y = 1;
+const int ZFIGURE_COORD_4_Y = 1;
+
+//coord square figure 
+const int JFIGURE_COORD_1_X = 0;
+const int JFIGURE_COORD_2_X = 0;
+const int JFIGURE_COORD_3_X = 0;
+const int JFIGURE_COORD_4_X = -1;
+
+const int JFIGURE_COORD_1_Y = 0;
+const int JFIGURE_COORD_2_Y = 1;
+const int JFIGURE_COORD_3_Y = 2;
+const int JFIGURE_COORD_4_Y = 2;
+
+//coord square figure 
+const int LFIGURE_COORD_1_X = 0;
+const int LFIGURE_COORD_2_X = 0;
+const int LFIGURE_COORD_3_X = 0;
+const int LFIGURE_COORD_4_X = 1;
+
+const int LFIGURE_COORD_1_Y = 0;
+const int LFIGURE_COORD_2_Y = 1;
+const int LFIGURE_COORD_3_Y = 2;
+const int LFIGURE_COORD_4_Y = 2;
+
+//coord square figure 
+const int SFIGURE_COORD_1_X = 0;
+const int SFIGURE_COORD_2_X = 1;
+const int SFIGURE_COORD_3_X = 0;
+const int SFIGURE_COORD_4_X = -1;
+
+const int SFIGURE_COORD_1_Y = 0;
+const int SFIGURE_COORD_2_Y = 0;
+const int SFIGURE_COORD_3_Y = 1;
+const int SFIGURE_COORD_4_Y = 1;
 
 const int VALUE_ROTATION = 3;
 
