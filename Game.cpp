2b8a9f53730189b73e83d9b9c87138ab8248d103
shinespeed <42372037+shinespeed@users.noreply.github.com>
2020-04-27 07:34:22 +0300

    UPDATE

--- Game.cpp
+++ Game.cpp
@@ -1,9 +1,20 @@
-﻿// Copyright 2009-2014 Blam BaseApps, Inc. All Rights Reserved.
+// Copyright 2009-2014 Blam BaseApps, Inc. All Rights Reserved.
 
 #include "Game.h"
 
+enum numberFigure
+{
+	TFIGURE,
+	QFIGURE,
+	IFIGURE,
+	ZFIGURE,
+	JFIGURE,
+	LFIGURE,
+	SFIGURE
+};
+
 Game::Game():
-	Parent(100, 80),
+	Parent(40, 20),
 	mDirection(true),
 	timeStep(0),
 	resolutionCreateFigure(true),
@@ -17,19 +28,37 @@ void Game::KeyPressed(int btnCode)
 	{
 		world.rotationFigure();
 	}
-	else if (btnCode == 115) //s
+	else if ((btnCode == 115)&&(world.regulationFigure(moveX, moveY + 1))) //s
 	{
 
 	}
-	else if (btnCode == 97) //a
+	else if ((btnCode == 75) && (world.regulationFigure(moveX - 1, moveY))) //a
 	{
 		--moveX;
 		world.moveFigure(moveX, moveY);
 	}
-	else if (btnCode == 100) //
+	else if ((btnCode == 77) && (world.regulationFigure(moveX + 1, moveY)))
 	{
 		++moveX;
 		world.moveFigure(moveX, moveY);
+	}
+}
+
+void Game::graphBorders() 
+{
+	for (int i = 0; i <= BORDER_END_X; i++)
+	{
+		for (int j = 0; j <= BORDER_END_Y; j++)
+		{
+			if ( i == 0 || j == 0 || i == BORDER_END_X || j == BORDER_END_Y || (i == BORDER_1_X)&&(j <= BORDER_1_Y) || (j == BORDER_2_Y))
+			{
+				SetChar(i, j, L'#');
+			}
+			else
+			{
+				//SetChar(i, j, 10);
+			}
+		}
 	}
 }
 
@@ -41,33 +70,22 @@ void Game::Render()
 		{
 			if (world.arrSquare[i][j] == nullptr) 
 			{
-				SetChar(j, i, L'.');
+				SetChar(j + PLAYING_FIELD_X, i + PLAYING_FIELD_X, L'.');
 			}
 			else 
 			{
-				SetChar(j, i, 10);
+				SetChar(j + PLAYING_FIELD_X, i + PLAYING_FIELD_Y, 10);
 			}
 		}
 	}
-}
-
-void Game::start() 
-{
-	createFigure();
-
-	if (timeStep >= GAME_ITERATION)
-	{
-		moveY++;
-		world.moveFigure(moveX, moveY);
-		timeStep = 0;
-	}
+	graphBorders();
 }
 
 void Game::createFigure()
 {
 	if (resolutionCreateFigure == true)
 	{
-		world.addFigure(new TFigure(START_COORD_X, START_COORD_Y));
+		world.addFigure(randomFigure());
 
 		moveX = START_COORD_X;
 		moveY = START_COORD_Y;
@@ -76,9 +94,61 @@ void Game::createFigure()
 	}
 }
 
+Figure* Game::randomFigure() 
+{
+	srand(time(NULL));                   
+
+	int random = rand() % 7;
+
+	switch (random)
+	{
+	case TFIGURE:
+		return new IFigure(START_COORD_X, START_COORD_Y);
+		break;
+	case QFIGURE:
+		return new QFigure(START_COORD_X, START_COORD_Y);
+		break;
+	case IFIGURE:
+		return new IFigure(START_COORD_X, START_COORD_Y);
+		break;
+	case ZFIGURE:
+		return new ZFigure(START_COORD_X, START_COORD_Y);
+		break;
+	case JFIGURE:
+		return new JFigure(START_COORD_X, START_COORD_Y);
+		break;
+	case LFIGURE:
+		return new LFigure(START_COORD_X, START_COORD_Y);
+		break;
+	case SFIGURE:
+		return new SFigure(START_COORD_X, START_COORD_Y);
+		break;
+	}
+}
+
+void Game::start()
+{
+	createFigure();
+
+	if (timeStep >= GAME_ITERATION)
+	{
+		if (world.regulationFigure(moveX, moveY + 1))
+		{
+			moveY++;
+		}
+		else
+		{
+			resolutionCreateFigure = true;
+		}
+		world.moveFigure(moveX, moveY);
+		timeStep = 0;
+	}
+}
+
 void Game::Update(float deltaTime)
 {
-       timeStep += deltaTime;
-       start();
-       Render();
+	timeStep += deltaTime;
+
+    start();
+	Render();
 }
